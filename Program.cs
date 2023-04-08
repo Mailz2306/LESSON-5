dfa79b19fffe0101ffc1bb9b0d3f9a151abe479b
mailz <sergey4istyak@yandex.ru>
2023-04-08 13:23:22 +0400

    дом задание 1 решено

--- Program.cs
+++ Program.cs
@@ -77,7 +77,7 @@ void PrintArray(int[] arr)
 }
 
 ДОМАШНЕЕ ЗАДАНИЕ 
-*/ 
+
 
 Задача 34: Задайте массив заполненный случайными положительными трёхзначными числами. Напишите программу, которая покажет количество чётных чисел в массиве.
 [345, 897, 568, 234] -> 2
@@ -90,3 +90,30 @@ _____________________________
 
 ЗАДАЧА 34: РЕШЕНИЕ
 
+*/
+Console.Write("Задайте длину массива: ");
+int a = int.Parse(Console.ReadLine());
+int[] randomArray = new int[a];
+
+void array(int a)
+{
+    for (int i = 0; i < a; i++)
+    {
+        randomArray[i] = new Random().Next(99, 999);
+        Console.Write(randomArray[i] + " ");
+    }
+}
+
+int amount(int[] randomArray)
+{
+    int amount = 0;
+    for (int i = 0; i < randomArray.Length; i++)
+    {
+        if (randomArray[i] % 2 == 0)
+            amount = amount + 1;
+    }
+    return amount;
+}
+
+array(a);
+Console.Write($"\nЧётных чисел в массиве: { amount(randomArray)}");
